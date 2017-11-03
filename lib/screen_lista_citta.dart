98fc57d1affa49493e8a27832046501892282800
Ivaskuu <ivascuadrian9+github@gmail.com>
2017-11-03 22:42:13 +0100

    Floating action bar

--- lib/screen_lista_citta.dart
+++ lib/screen_lista_citta.dart
@@ -36,18 +36,39 @@ class CopertinaCitta extends StatelessWidget
         image: new DecorationImage
         (
           image: new AssetImage(_img),
-          fit: BoxFit.cover
+          fit: BoxFit.cover,
+          colorFilter: new ColorFilter.mode(Colors.black45, BlendMode.luminosity)
         )
       ),
-      child: new Align
+      child: new Stack
       (
-        alignment: FractionalOffset.topLeft,
-        child: new Container
-        (
-          margin: new EdgeInsets.only(top: 32.0, left: 24.0, right: 24.0),
-          child: new Text(_nomeCitta.toUpperCase(), style: new TextStyle(color: Colors.white, fontSize: 90.0, fontWeight: FontWeight.w700)),
-        )
-      ),
+        children: <Widget>
+        [
+          new Align
+          (
+            alignment: FractionalOffset.topLeft,
+            child: new Container
+            (
+              margin: new EdgeInsets.only(top: 32.0, left: 24.0, right: 24.0),
+              child: new Text(_nomeCitta.toUpperCase(), style: new TextStyle(color: Colors.white, fontSize: 90.0, fontWeight: FontWeight.w700)),
+            )
+          ),
+          new Align
+          (
+            alignment: FractionalOffset.bottomCenter,
+            child: new Container
+            (
+              margin: new EdgeInsets.all(16.0),
+              child: new FloatingActionButton
+              (
+                backgroundColor: Colors.white,
+                child: new Icon(Icons.keyboard_arrow_up, color: Colors.black),
+                onPressed: () => print("Click")
+              )
+            )
+          ),
+        ],
+      )
     );
   }
 }
